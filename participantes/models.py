3aaf0f28c87241a5bd706c18fe2d3f4404f5403b
Ednaldo Batista Barreto <suporte@implanti.com.br>
2018-12-07 10:46:29 -0300

    Correção de campos
    Corrigido alguns campos do banco de dados

--- participantes/models.py
+++ participantes/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class Participantes(models.Model):
-    COD_PART = models.CharField(max_length=60, primary_key=True, null=False, blank=False, unique=True)
-    NOME = models.CharField(max_length=100, null=False, blank=False, unique=True)
-    COD_PAIS = models.CharField(max_length=5, null=False, blank=False, unique=True)
+    COD_PART = models.CharField(max_length=60, primary_key=True, null=False, blank=False)
+    NOME = models.CharField(max_length=100, null=False, blank=False)
+    COD_PAIS = models.CharField(max_length=5, null=False, blank=False)
     CNPJ = models.CharField(max_length=14)
     CPF = models.CharField(max_length=11)
     IE = models.CharField(max_length=14)
     COD_MUN = models.CharField(max_length=7)
     SUFRAMA = models.CharField(max_length=9)
-    END = models.CharField(max_length=60, null=False, blank=False, unique=True)
+    END = models.CharField(max_length=60, null=False, blank=False)
     NUM = models.CharField(max_length=10)
     COMPL = models.CharField(max_length=60)
     BAIRRO = models.CharField(max_length=60)
